a68141d406b8f657892495c5c27ad1e698598a7c
Gabriela Rodrigues <gabriela.matias@sou.inteli.edu.br>
2023-11-17 15:56:43 -0300

    update

--- actions.py
+++ actions.py
@@ -1,6 +1,6 @@
 resolution = {
-        "Atualizar método de pagamento": "",
-        "Acompanhar Status do meu Pedido":"", 
+        "Atualizar método de pagamento": payment(),
+        "Acompanhar Status do meu Pedido":status(), 
         }
 
 def payment(resolution):
@@ -10,7 +10,7 @@ def payment(resolution):
         print("Não encontramos uma resposta para sua pergunta. Pode tentar reescreve-la, por favor")
 
 def status(resolution):
-    if resolution == "Atualizar método de pagamento":
-        print(f"Para atualizar seu método de pagamento basta acessar: www.abc.com e entrar na aba 'ABC' ")
+    if resolution == "Status do Pedido":
+        print(f"Para Acompanhar o Status do Seu pedido basta acessar: www.abc.com e entrar na aba 'ABC' ")
     else: 
         print("Não encontramos uma resposta para sua pergunta. Pode tentar reescreve-la, por favor")
--- simpleChat.py
+++ simpleChat.py
@@ -2,16 +2,16 @@ import re
 from . import actions
 
 intent_dict = {
-        r"\b[Ss][tatus](?:\spedido)?\s?[oa]?\s(?:\srastrear)?\s?[oa]?\s(.+)(?:\sonde)?\s?[oa]?\s": actions.status,
-        r"\b[Aa][](?:\spara)?\s?[oa]?\s(.+)": actions.payment
+        r"\b[Aa][tatus](?:\spedido)?\s?[oa]?\s(?:\srastrear)?\s?[oa]?\s(.+)(?:\sonde)?\s?[oa]?\s": actions.status,
+        r"\b[Aa](?:\smudar)?\s?[oa]?\s(.+)\b[Aa](?:\spagamento)?\s?[oa]?\s(.+)": actions.payment
         }
 
 def main():
     command = input("Digite o sua pergunta: ")
     for key, function in intent_dict.items():
         pattern = re.compile(key)
-        point = pattern.findall(command)
-        if point:
+        resolution_user = pattern.findall(command)
+        if resolution_user:
             print("Encontrei uma intenção! Computando...")
             function(point[0])
             break
